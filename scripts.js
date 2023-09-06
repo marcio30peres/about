be7c0d61f9f6b43ef2ce0890d4dceacb4f3d7ab8
Márcio <marcio30peres@gmail.com>
2023-09-05 22:57:02 -0300

    add: scripts file

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function returnTyped() {
+  return new Typed("#auto-typing", {
+    strings: [
+      "^500 System developer^500",
+      "^500 HTML, CSS, JS, PHP^500",
+      "^500 React, Nest, Laravel^500",
+      "^500 Git, Github^500",
+      "^500 Advanced English^500",
+      "^500 Contact me!^500",
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+  })
+}
